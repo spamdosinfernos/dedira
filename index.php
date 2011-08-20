34262ac483ef33f0259cb087a0f488ee7f4a62a6
tatupheba <tatupheba@el-micrito>
2011-08-20 01:36:47 -0300

    Adicionei o arquivo que vai gerenciar todas as requisições do sistema

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . 'Class/Core/CCore.php';
+
+/**
+ * Esta classe gerencia todas as requisições recebidas pelo site
+ * Requisições enviadas a outros arquivos devem ser ignoradas
+ * @author tatupheba
+ *
+ */
+class index extends CCore{
+	
+}
+?>
