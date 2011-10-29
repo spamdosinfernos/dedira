faff060904060e9f382e7776c0c73f92d321a129
André <tatupheba@el-netbookito.(none)>
2011-10-29 21:56:03 -0200

    Terminei de fazer um grid simples com edição no extjs, esta será a abiblioteca padrão para interfaces no milisytem

--- sandBox/extjs/Teste/app.js
+++ sandBox/extjs/Teste/app.js
@@ -2,8 +2,12 @@ Ext.application( {
 	name : 'AM',
 	appFolder : 'app',
 
+	stores : [ 'Users' ],
+
 	// Anexa os controladores do sistema
 	controllers : [ 'Users' ],
+
+	models : [ 'User' ],
 
 	launch : function() {
 
--- sandBox/extjs/Teste/app/controller/Users.js
+++ sandBox/extjs/Teste/app/controller/Users.js
@@ -10,15 +10,32 @@ Ext.define('AM.controller.Users', {
 
 	init : function() {
 
-	// Detecta os eventos das views
+		// Detecta os eventos das views
 	this.control( {
-		'userlist' : {
+		'viewport > userlist' : {
 			// Detecta evento render
 			render : this.onPanelRendered,
 			// Detecta evento duplo clique
 			itemdblclick : this.editUser
+		},
+		// Quando um botão cuja ação for "save" for clicado dispara o evento
+		// de salvamento
+		'useredit button[action=save]' : {
+			click : this.updateUser
 		}
 	});
+},
+
+updateUser : function(button) {
+	console.log('O botão salvar foi clicado!');
+	var win = button.up('window');
+	var form = win.down('form');
+	var record = form.getRecord();
+
+	var values = form.getValues();
+
+	record.set(values);
+	win.close();
 },
 
 onPanelRendered : function() {
