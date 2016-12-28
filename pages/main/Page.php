f7a62fa876613714c934329d8ada6c389d521d68
André Furlan <ensismoebius@gmail.com>
2016-12-28 12:39:06 -0200

    Pure css project added
    Pure css was the because it is lightweight an modularized.
    Seens a good alternative for bootstrap or materialize

--- pages/main/Page.php
+++ pages/main/Page.php
@@ -24,14 +24,60 @@ class Page implements \IPage {
 		
 		$this->xTemplate = new \TemplateLoader ( Conf::getTemplate () );
 		$this->xTemplate->assign ( "wellcomeMessage", __ ( "Hello" ) );
-		
 		$this->xTemplate->assign ( "userName", $user->getName () . " " . $user->getLastName () );
+		
+		$this->createMenu ();
 		
 		$this->xTemplate->parse ( "main" );
 		$this->xTemplate->out ( "main" );
 	}
 	public static function isRestricted(): bool {
 		return true;
+	}
+	public function createMenu() {
+		$this->createSuggestionsMenuEntry ();
+		$this->createPrioritiesMapMenuEntry();
+		$this->createRankingMenuEntry();
+		$this->createRateMenuEntry();
+		$this->createRulesMenuEntry();
+		$this->createSuggestionsMenuEntry();
+		$this->createVoteMenuEntry();
+	}
+	public function createSuggestionsMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Suggestions" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=suggestions" );
+		$this->xTemplate->assign ( "updatesAmount", 0 );
+		$this->xTemplate->parse ( "main.menu" );
+	}
+	public function createRateMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Rate" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=rate" );
+		$this->xTemplate->assign ( "updatesAmount", 0 );
+		$this->xTemplate->parse ( "main.menu" );
+	}
+	public function createVoteMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Vote" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=vote" );
+		$this->xTemplate->assign ( "updatesAmount", 0 );
+		$this->xTemplate->parse ( "main.menu" );
+	}
+	public function createRankingMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Ranking" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=ranking" );
+		$this->xTemplate->assign ( "updatesAmount", 0 );
+		$this->xTemplate->parse ( "main.menu" );
+	}
+	public function createRulesMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Rules" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=rules" );
+		$this->xTemplate->assign ( "updatesAmount", "" );
+		$this->xTemplate->parse ( "main.menu" );
+	}
+	public function createPrioritiesMapMenuEntry() {
+		$this->xTemplate->assign ( "menuText", __ ( "Priorities map" ) );
+		$this->xTemplate->assign ( "menuAddress", "index.php?page=prioritiesMap" );
+		$this->xTemplate->assign ( "updatesAmount", 0 );
+		$this->xTemplate->parse ( "main.menu" );
 	}
 }
 ?>
